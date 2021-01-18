eea55946d6ec6033cf3e5552f37fa785cad9883c
Jay Idrees <idreesjay@gmail.com>
2021-01-17 22:32:22 -0500

    Updating the shell file scrypt for the lucky-duck investigation

---
+++ lucky-duck.sh
@@ -0,0 +1,36 @@
+
+## Creating files and folders
+
+mkdir Lucky_Duck_Investigations
+
+# Creating company's folder
+
+cd Lucky_Duck_Investigations
+
+# Creating a subfolder for a specific investigation
+
+mkdir Roulette_Loss_Investigation 
+
+cd Roulette_Loss_Investigation
+
+# Creating Individual analysis folder
+
+mkdir Player_Analysis
+mkdir Dealer_Analysis
+mkdir Player_Dealer_Correlation
+
+# Creating Notes file in every directory created above
+
+cd Player_Analysis
+touch Notes_Player_Analysis
+cd ../
+cd Dealer_Analysis
+touch Notes_Dealer_Analysis
+cd ../
+cd Player_Dealer_Correlation
+touch Notes_Player_Dealer_Correlation
+cd /Lucky_Duck_Investigations
+
+# Moving back to the Luky_Dealer_Investigation directory
+cd ../../
+
